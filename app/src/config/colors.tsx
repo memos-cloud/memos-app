b30cc13e8cb3efc83e5a413a90f1a26aead2fb0d
YassinEldeeb <yassineldeeb94@gmail.com>
2021-08-21 00:59:56 +0200

    Better Login Screen

--- app/src/config/colors.tsx
+++ app/src/config/colors.tsx
@@ -1,5 +1,7 @@
 export const colors = {
   primary: '#FF4C29',
+  google: '#ea4335',
+  facebook: '#1a538a',
   secondary: '#181818',
   black: '#0F0F0F',
   white: '#ffffff',
--- app/src/screens/LoginScreen.tsx
+++ app/src/screens/LoginScreen.tsx
@@ -1,40 +1,68 @@
 import React from 'react'
-import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
-import { Center } from '../components/Center'
+import { ImageBackground, StyleSheet, View } from 'react-native'
 import Container from '../components/Container'
 import { MyButton } from '../components/MyButton'
-import { MyText } from '../components/MyText'
 import { colors } from '../config/colors'
+import { LinearGradient } from 'expo-linear-gradient'
+import { MyText } from '../components/MyText'
 
 export function LoginScreen() {
   return (
-    <>
-      <ImageBackground
-        resizeMode='contain'
-        style={styles.loginImage}
-        source={require('../assets/Images/login.png')}
-      ></ImageBackground>
-
-      <Container customStyles={{ paddingVertical: 0 }}>
-        <MyButton
-          customStyles={styles.loginBtn}
-          bg={colors.primary}
-          text='Continue With Google'
-          icon={require('../assets/Images/google.png')}
-          onPress={() => {}}
-        />
-      </Container>
-    </>
+    <ImageBackground
+      style={styles.backgroundImage}
+      source={require('../assets/Images/login.png')}
+    >
+      <LinearGradient
+        style={{ flex: 1 }}
+        start={[0, 0]}
+        end={[1, 1]}
+        colors={['#0F0F0F', 'rgba(15, 15, 15, 0.2)']}
+      >
+        <Container
+          customStyles={{
+            flex: 1,
+            justifyContent: 'space-between',
+            backgroundColor: null as any,
+          }}
+        >
+          <MyText customStyles={styles.backgroundOverlay}>
+            Manage and Sync your best moments with Memos.
+          </MyText>
+          <View>
+            <MyButton
+              customStyles={styles.loginBtn}
+              bg={colors.google}
+              text='Continue With Google'
+              icon={require('../assets/Images/google.png')}
+              onPress={() => {}}
+            />
+            <MyButton
+              customStyles={styles.loginBtn}
+              bg={colors.facebook}
+              text='Continue With Facebook'
+              icon={require('../assets/Images/facebook.png')}
+              onPress={() => {}}
+            />
+          </View>
+        </Container>
+      </LinearGradient>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
+  backgroundOverlay: {
+    fontSize: 35,
+    borderLeftColor: 'rgba(255, 77, 41, 0.8)',
+    borderLeftWidth: 4,
+    paddingLeft: 20,
+    marginTop: 30,
+  },
   loginBtn: {
     width: '100%',
   },
-  loginImage: {
-    width: '100%',
-    height: 175,
-    backgroundColor: colors.black,
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
   },
 })
