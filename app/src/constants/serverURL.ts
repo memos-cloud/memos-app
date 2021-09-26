bb53ea82027285ac1ff1d9c46625637a77070720
YassinEldeeb <yassineldeeb94@gmail.com>
2021-09-26 13:15:09 +0200

    fix serverURL

--- app/src/constants/serverURL.ts
+++ app/src/constants/serverURL.ts
@@ -1,4 +1,4 @@
 export const serverURL =
   process.env.NODE_ENV === 'development'
     ? 'http://192.168.1.5.sslip.io:3000'
-    : 'http://192.168.1.5.sslip.io:3000'
+    : 'https://memos-rn.herokuapp.com'
